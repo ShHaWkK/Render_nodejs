f474af25ef4746ef69937fe27ad8d347af08b3f1
ShHaWkK <alexandreuzan9@gmail.com>
2024-02-27 10:55:51 +0100

    Update file (for render)

--- db.js
+++ db.js
@@ -2,11 +2,12 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
+  process.env.DB_NAME, // Database
+  process.env.DB_USER, // Username
+  process.env.DB_PASS, // Password
+  
   {
-    host: '', // Host
+    host: process.env.DB_HOST, // Host
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
